e06c8f14daf0d278902da0c940c166465fe7a5bd
kaushaln <nayakkaushal.ahd@gmail.com>
2020-05-11 18:42:55 +0530

    First Commit in Core Java Practice

---
+++ Practice/src/corejava/simple/HelloWorld.java
@@ -0,0 +1,12 @@
+// Write a simple program to display "Hello World" text in console.
+
+package corejava.simple;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+	}
+
+}
---
+++ Practice/src/corejava/simple/PrintMethods.java
@@ -0,0 +1,20 @@
+//Write a program to display difference of print and println method
+package corejava.simple;
+
+public class PrintMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Print method 
+		System.out.print("Hello World");
+		System.out.print(" Greetings from Human of Earth");
+		
+		//println method 
+		System.out.println("\nHello world");
+		System.out.println("Greetings from Human of Earth");
+
+	}
+
+}
